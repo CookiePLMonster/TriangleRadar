bffc1c007b942b573497a992970441a965a833aa
Silent <zdanio95@gmail.com>
2017-03-31 01:14:22 +0200

    Cleaned up
    Removed radardisc

--- dllmain.cpp
+++ dllmain.cpp
@@ -5,6 +5,8 @@
 #include "MemoryMgr.h"
 #include "Maths.h"
 
+static void (*const TransformRadarPointToScreenSpace)(CVector2D&,const CVector2D&) = (void(*)(CVector2D&,const CVector2D&))0x583480;
+
 double TriangleRadar(CVector2D& pos)
 {
 	double	dResult = sqrt(pos.x * pos.x + pos.y * pos.y);
@@ -37,6 +39,7 @@ double TriangleRadar(CVector2D& pos)
 	return dResult;
 }
 
+static void* const TriangleRadarMask_JumpBack = (void*)0x585888;
 void __declspec(naked) TriangleRadarMask()
 {
 	static const float fPositions[] = { 0.0, 1.0, 0.0, 1.0, 1.0, -1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0 };
@@ -46,23 +49,22 @@ void __declspec(naked) TriangleRadarMask()
 		lea		ecx, [esp+38h]
 		push	eax
 		push	ecx
-		mov		eax, 583480h
-		call	eax
+		call	TransformRadarPointToScreenSpace
+
 		lea		eax, [ebx*8+fPositions+8]
 		lea		ecx, [esp+48h]
 		push	eax
 		push	ecx
-		mov		eax, 583480h
-		call	eax
+		call	TransformRadarPointToScreenSpace
+
 		lea		eax, [ebx*8+fPositions+18h]
 		lea		ecx, [esp+58h]
 		push	eax
 		push	ecx
-		mov		eax, 583480h
-		call	eax
+		call	TransformRadarPointToScreenSpace
+
 		add		esp, 18h
-		push	585888h
-		retn
+		jmp		TriangleRadarMask_JumpBack
 	}
 }
 
@@ -84,6 +86,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 			Patch<uint32_t>(0x58581A + 1, 2);
 			Patch<uint8_t>(0x585894 + 1, 3); // Num of vertices
 			Patch<uint8_t>(0x58589B + 1, 3);
+
+			// No radardisc
+			InjectHook(0x58A782, 0x58AA2A, PATCH_JUMP );
 		}
 	}
 	return TRUE;
